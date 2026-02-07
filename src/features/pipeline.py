7c0dd64358b2a9432356e2fae51550385eeaebd2
shrys <shreyasjain2018@gmail.com>
2026-02-07 13:08:44 +0530

    add pipline for num, cat transformation

--- src/features/pipeline.py
+++ src/features/pipeline.py
@@ -1,1 +1,37 @@
 """Feature pipeline orchestration."""
+
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.impute import SimpleImputer
+
+
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.impute import SimpleImputer
+
+
+def build_preprocessing_pipeline(num_cols, cat_cols):
+
+    
+    numeric_pipeline = Pipeline(steps=[
+        ("imputer", SimpleImputer(strategy="median")),
+        ("scaler", StandardScaler())
+    ])
+
+ 
+    categorical_pipeline = Pipeline(steps=[
+        ("imputer", SimpleImputer(strategy="most_frequent")),
+        ("encoder", OneHotEncoder(handle_unknown="ignore"))
+    ])
+
+  
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ("num", numeric_pipeline, num_cols),
+            ("cat", categorical_pipeline, cat_cols)
+        ]
+    )
+
+    return preprocessor
