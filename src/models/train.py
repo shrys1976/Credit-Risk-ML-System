b8e7721b45efd5822d9a732fbd730d2c1288ca90
shrys <shreyasjain2018@gmail.com>
2026-02-07 15:21:12 +0530

    Add logistic regression baseline training pipeline integrating preprocessing and model training

--- src/models/train.py
+++ src/models/train.py
@@ -1,1 +1,23 @@
 """Train credit risk models."""
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import roc_auc_score
+
+
+def train_logistic_model(preprocessor, X_train, y_train):
+    model_pipeline  = Pipeline(steps=[
+
+        ("preprocessor", preprocessor),
+        ("model", LogisticRegression(max_iter=1000))
+
+    ])
+
+    
+    model_pipeline.fit(X_train, y_train)
+    return model_pipeline
+
+    
+
+
+
